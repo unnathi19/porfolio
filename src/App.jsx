7b349ccf0fa96d7e3689ebdedcabbac1c297d3a7
Unnathi <unnathi.ext@accord-soft.com>
2025-09-15 15:15:04 +0530

    headers

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ import About from "./components/About";
 import Project from "./components/Project";
 import Skills from "./components/Skills";
 import Experience from "./components/Experience";
-import Header from "./components/header";
+import Header from "./components/headers";
 function App() {
   return (
     <div className="text-black bg-black scroll-smooth">
